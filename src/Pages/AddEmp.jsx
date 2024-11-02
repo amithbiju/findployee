1129ccf8968b2deec5077598581cb34362469e9e
Amith Biju <amithbiju1@gmail.com>
2024-11-02 20:29:44 +0530

    dash_update

--- src/Pages/AddEmp.jsx
+++ src/Pages/AddEmp.jsx
@@ -65,6 +65,7 @@ function AddEmp() {
       empid,
       dept,
       email,
+      exp,
     }).then(() => {
       addDoc(collection(db, "skills"), {
         empid,
@@ -219,6 +220,7 @@ function AddEmp() {
                   autoComplete="country-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm/6"
                 >
+                  <option>Select</option>
                   <option>Quality assurance</option>
                   <option>project Development</option>
                   <option>Testing</option>
