8d8d10cd4407fdc8968c7a74444f11fd0b26a2d1
TheLycan <ethanharry30@gmail.com>
2024-11-02 17:50:32 +0530

    modifie:test.js

--- src/gemini/test.js
+++ src/gemini/test.js
@@ -1,15 +1,44 @@
-// Make sure to include these imports:
-//import { GoogleGenerativeAI } from "@google/generative-ai";
+
+require('dotenv').config();
+const KEY = process.env.API_KEY;
+
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI('AIzaSyBWmdD4ctq5M08Jz-C1E0POWbsn2vSlj2k');
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-const prompt = "Write a story about a magic backpack.";
+const { GoogleAIFileManager } = require ("@google/generative-ai/server");
 
 
-const result = async () =>{  
-    await model.generateContent(prompt).then(a => {
-        console.log(a.response.text())
-    });
-}
-console.log(result());
+// Initialize GoogleGenerativeAI with your API_KEY.
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+// Initialize GoogleAIFileManager with your API_KEY.
+const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({
+  // Choose a Gemini model.
+  model: "gemini-1.5-flash",
+});
+
+// Upload the file and specify a display name.
+
+const uploadResponse = async ()=> await fileManager.uploadFile("emp.txt", {
+  mimeType: "text/plain",
+  displayName: "Gemini 1.5 PDF",
+});
+
+// View the response.
+console.log(
+  `Uploaded file ${uploadResponse.file.displayName} as: ${uploadResponse.file.uri}`,
+);
+
+// Generate content using text and the URI reference for the uploaded file.
+
+const result = async () => await model.generateContent([
+  {
+    fileData: {
+      mimeType: uploadResponse.file.mimeType,
+      fileUri: uploadResponse.file.uri,
+    },
+  },
+  { text: "Identify the best employee(s) for a task that requires strong Python and Machine Learning skills." },
+]);
+
+// Output the generated text to the console
+console.log(result.response.text());
