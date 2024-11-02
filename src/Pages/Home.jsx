6090f6205033bd9a93a42a894cd57cf348239b9c
TheLycan <ethanharry30@gmail.com>
2024-11-02 20:17:59 +0530

    get started to dashboard

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
             </h4>
             <div class="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="/dashboard"
                 class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
