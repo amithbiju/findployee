318ca366905fa32abe812be5e0f952b846cea8d1
Debilu <devanarayansuniloffl@gmail.com>
2024-11-03 09:42:37 +0530

    visual and click updates

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -29,16 +29,16 @@ const Home = () => {
 
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="/scanner"
+                href="/dashboard"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Dashboard
               </a>
               <a
-                href="#"
+                href="/createTeam"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
-                learn more <span aria-hidden="true">→</span>
+                Create your team now <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -24,11 +24,6 @@ Navigate("/")
     {/* {userLoggedIn && (<Navigate to={'/Home'} replace={true} />)} */}
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow items-center justify-center dark:border md:mt-0 sm:max-w-md xl:p-0">
-        <img
-            alt="Your Company"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
-          />
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                     Login to your account
